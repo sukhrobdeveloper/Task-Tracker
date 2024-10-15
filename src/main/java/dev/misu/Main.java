e71a5200dd88708428b985aa8d2d9527b18d8cd6
sukhrobdeveloper <sukhrobtokhirov006@gmail.com>
2024-10-15 23:17:53 +0500

    Businness Logic

--- src/main/java/dev/misu/Main.java
+++ src/main/java/dev/misu/Main.java
@@ -1,7 +1,30 @@
 package dev.misu;
 
+import dev.misu.controller.TaskController;
+import dev.misu.controller.TaskControllerImpl;
+import dev.misu.entities.Task;
+
 public class Main {
+
     public static void main(String[] args) {
-        
+
+        TaskController taskController = new TaskControllerImpl();
+
+        if (args.length < 2) {
+            System.out.println("Usage: task-cli <command> <parameters>");
+            return;
+        }
+
+        String command = args[0];
+        switch (command) {
+            case "add":
+                String taskRes = taskController.createTask(args[1]);
+                System.out.println(taskRes);
+                break;
+            case "update":
+                int id = Integer.parseInt(args[1]);
+                
+        }
+
     }
 }
